6a25ae980141c758d1a310fbedcefe3e4decc7e9
AlexanderWestman <superalle96@gmail.com>
2025-06-19 13:34:50 +0200

    Update points.ts

--- src/lib/points.ts
+++ src/lib/points.ts
@@ -11,7 +11,24 @@ export const DMI_GRID_POINTS = [
     { lat: 55.541006, lon: 12.824463 }, // Längre söderut i Öresund
     { lat: 55.484650, lon: 12.395566 }, // Sydväst om Köge, Kattegatt
     { lat: 55.793953, lon: 12.742530 }, // Generell punkt i Öresund
-    
+    { lat: 55.654009, lon: 12.675558 },  // Öresund öster om Köpenhamn
+    { lat: 55.376614, lon: 12.749274 },  // Söder om Saltholm
+    { lat: 55.372902, lon: 12.481441 },  // Syd om Själland
+    { lat: 56.101938, lon: 12.500313 },  // Kattegatt norr om Helsingborg
+    { lat: 56.363815, lon: 11.728910 },  // Kattegatt västerut
+    { lat: 56.434113, lon: 12.477218 },  // Kattegatt nordväst
+    { lat: 56.231117, lon: 12.460260 },  // Kattegatt central punkt
+    { lat: 56.208441, lon: 12.367038 },  // Kattegatt öster om Gilleleje
+    { lat: 56.183589, lon: 12.291295 },  // Kattegatt nära gräns Danmark/Sverige
+    { lat: 55.9966337, lon: 12.60389 },
+    { lat: 56.086676, lon: 12.60389  },
+    { lat: 56.080716, lon: 12.584469 },
+    { lat: 56.046369, lon: 12.679508 },
+    { lat: 56.045602, lon: 12.659766 },
+    { lat: 56.039273, lon: 12.626807 },
+    { lat: 56.003968, lon: 12.651527 },
+    { lat: 56.002912, lon: 12.607066 }
+
 
 
 
